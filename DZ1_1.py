0cb340ed4a3c116749ccb38ad5e8c23785467009
MikhailV <Ratven@yandex.ru>
2021-02-12 23:10:06 +0300

    Added Task1_1

---
+++ DZ1_1.py
@@ -0,0 +1,18 @@
+duration = int(input("Введите время в секундах: "))
+
+days = duration // (24 * (60**2))
+duration -= days * (24 * (60**2))
+hours = duration // 60 ** 2
+duration -= hours * 60 ** 2
+minutes = duration // 60
+seconds = duration % 60
+
+if days != 0:
+    print(days, "дн", hours, "час", minutes, "мин", seconds, "сек")
+elif hours != 0:
+    print(hours, "час", minutes, "мин", seconds, "сек")
+elif minutes != 0:
+    print(minutes, "мин", seconds, "сек")
+else:
+    print(seconds, "сек")
+
